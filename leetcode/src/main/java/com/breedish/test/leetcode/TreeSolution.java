5ffef71970b024bd8eab4a09b44dc0d82059af30
Dzmitriy Zenin <d.zenin@severex.io>
2020-02-10 10:57:53 +0300

    123

--- leetcode/src/main/java/com/breedish/test/leetcode/TreeSolution.java
+++ leetcode/src/main/java/com/breedish/test/leetcode/TreeSolution.java
@@ -1,5 +1,11 @@
 package com.breedish.test.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class TreeSolution {
 
     public static class TreeNode {
@@ -12,8 +18,78 @@ public class TreeSolution {
         }
     }
 
-    public boolean isValidBST(TreeNode root) {
-        return true;
+    public boolean isValidBSTV1(TreeNode root) {
+        return isValidNode(root, null, null);
+    }
+
+    private boolean isValidNode(TreeNode node, Integer min, Integer max) {
+        if (node == null) return true;
+
+        if (min != null && node.val <= min) return false;
+        if (max != null && node.val >= max) return false;
+
+        return isValidNode(node.left, min, node.val) && isValidNode(node.right, node.val, max);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return t1.val == t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        List<Integer> level = new ArrayList<>();
+        List<TreeNode> nextLevel = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            level.add(node.val);
+
+            if (node.left != null) nextLevel.add(node.left);
+            if (node.right != null) nextLevel.add(node.right);
+
+            if (queue.isEmpty()) {
+                levels.add(level);
+                queue.addAll(nextLevel);
+
+                level = new ArrayList<>();
+                nextLevel.clear();
+            }
+        }
+
+        return levels;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) return levels;
+        traverse(root, levels, 0);
+        return levels;
+    }
+
+    private void traverse(TreeNode node, List<List<Integer>> levels, int lvl) {
+        if (node == null) return;
+        if (levels.size() <= lvl) levels.add(new LinkedList<>());
+
+        List<Integer> level = levels.get(lvl);
+        if (lvl % 2 == 0) {
+            level.add(node.val);
+        } else {
+            level.add(0, node.val);
+        }
+
+        traverse(node.left, levels, lvl + 1);
+        traverse(node.right, levels, lvl + 1);
     }
 
 }
--- leetcode/src/test/java/com/breedish/test/leetcode/TreeSolutionTest.java
+++ leetcode/src/test/java/com/breedish/test/leetcode/TreeSolutionTest.java
@@ -1,7 +1,12 @@
 package com.breedish.test.leetcode;
 
 import com.breedish.test.leetcode.TreeSolution.TreeNode;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class TreeSolutionTest {
 
@@ -9,6 +14,31 @@ public class TreeSolutionTest {
 
     @Test
     public void testIsValid() {
-        solution.isValidBST(new TreeNode(1));
+        Assert.assertThat(solution.isValidBSTV1(new TreeNode(1)), is(true));
+        TreeNode n1 = new TreeNode(2);
+        n1.left = new TreeNode(1);
+        n1.right = new TreeNode(3);
+        Assert.assertThat(solution.isValidBSTV1(n1), is(true));
     }
+
+    @Test
+    public void testOrderTraversals() {
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+
+        Stack<Integer> s = new Stack<Integer>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        for (Integer integer : s) {
+            System.out.println(integer);
+        }
+
+        var res = solution.zigzagLevelOrder(node);
+    }
+
+
 }
